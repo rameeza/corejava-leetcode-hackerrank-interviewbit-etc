80cbb3d078ecbb70e0b3775937142556b0857508
rameez <rameezahma@gmail.com>
2024-11-28 12:29:59 +0530

    interviewbit extracting-numbers

--- src/test/java/com/rameez/ExtractingNumbers.java
+++ src/test/java/com/rameez/ExtractingNumbers.java
@@ -8,6 +8,7 @@ import static org.junit.Assert.assertEquals;
 
 // https://www.interviewbit.com/problems/extracting-numbers/
 
+// Both solve(Sting A) and solveUsingStingBuilder(String A) work fine.
 public class ExtractingNumbers {
 
 
@@ -18,25 +19,51 @@ public class ExtractingNumbers {
 
         Long answer = 0L;
 
-
+        // get String array of numbers
         String[] numbers = A.replaceAll("[a-z]+", " ").trim().split(" ");
 
         for (int i = 0; i < numbers.length; i++) {
             answer += Integer.parseInt(numbers[i]);
         }
+        return answer;
+    }
 
+
+    public Long solveUsingStringBuilder(String A) {
+        Long answer = 0L;
+        StringBuilder sb = new StringBuilder("");
+
+        for (int i = 0; i < A.length(); i++) {
+            char c = A.charAt(i);
+            if (c >= '0' && c <= '9') {
+                sb.append(c);
+            } else if (sb.length() > 0) {
+                Integer no = Integer.parseInt(sb.toString());
+                answer += no;
+                sb = new StringBuilder("");
+            }
+        }
+        if (sb.length() > 0) {
+            Integer no = Integer.parseInt(sb.toString());
+            answer += no;
+            sb = new StringBuilder("");
+        }
         return answer;
     }
 
     @Test
     public void main() {
-//        assertEquals(solve("a12b34c"), 46L);
-
 
         solve("a12b34c");
         solve("abcde");
         solve("1000");
         solve("");
         solve(" ");
+
+        solveUsingStringBuilder("a12b34c");
+        solveUsingStringBuilder("abcde");
+        solveUsingStringBuilder("1000");
+        solveUsingStringBuilder("");
+        solveUsingStringBuilder(" ");
     }
 }
