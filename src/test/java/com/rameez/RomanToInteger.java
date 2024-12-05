79b961ab30c8951ded9f25e7ee99045bf25098d3
rameez <rameezahma@gmail.com>
2024-12-05 21:18:44 +0530

    leetcode roman-to-integer

--- src/test/java/com/rameez/RomanToInteger.java
+++ src/test/java/com/rameez/RomanToInteger.java
@@ -43,6 +43,33 @@ public class RomanToInteger {
         return answer;
     }
 
+    // APPROACH 2: EXECUTES FASTER
+    public int romanToInt2(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int answer = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            Character c = s.charAt(i);
+            if (map.get(c) >= map.get(s.charAt(i + 1))) {
+                answer += map.get(c);
+            } else {
+                answer -= map.get(c);
+
+            }
+        }
+        // add value of last character.
+        answer += map.get(s.charAt(s.length() - 1));
+        return answer;
+    }
+
+
     @Test
     public void main() {
         assertEquals(1994, romanToInt("MCMXCIV"));
