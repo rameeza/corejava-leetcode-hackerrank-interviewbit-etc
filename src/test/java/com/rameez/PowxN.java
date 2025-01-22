7573afc8360ad84d7ae78822e6b746576b088909
rameez <rameezahma@gmail.com>
2025-01-22 14:14:02 +0530

    leetcode powx-n

---
+++ src/test/java/com/rameez/PowxN.java
@@ -0,0 +1,17 @@
+package com.rameez;
+
+import org.junit.Test;
+
+// https://leetcode.com/problems/powx-n/
+
+public class PowxN {
+
+    public double myPow(double x, int n) {
+        return Math.pow(x, n);
+    }
+
+    @Test
+    public void main() {
+
+    }
+}
