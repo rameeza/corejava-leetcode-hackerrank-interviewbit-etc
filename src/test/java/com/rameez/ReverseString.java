a62680f3f232f3a1c62ef15b47a47bf8cafc17ec
rameez <rameezahma@gmail.com>
2025-01-29 20:48:49 +0530

    leetcode reverse-string

---
+++ src/test/java/com/rameez/ReverseString.java
@@ -0,0 +1,22 @@
+package com.rameez;
+
+import org.junit.Test;
+
+// https://leetcode.com/problems/reverse-string/
+
+public class ReverseString {
+
+    public void reverseString(char[] s) {
+        for (int i = 0; i < s.length / 2; i++) {
+            char temp = s[i];
+            s[i] = s[s.length - 1 - i];
+            s[s.length - 1 - i] = temp;
+        }
+
+    }
+
+    @Test
+    public void main() {
+
+    }
+}
