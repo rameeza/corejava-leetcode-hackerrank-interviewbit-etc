ccf0d75c16369b17fba951881f2c72ba23f9a2da
rameez <rameezahma@gmail.com>
2025-02-27 16:19:27 +0530

    leetcode reverse-bits

--- src/test/java/com/rameez/ReverseBits.java
+++ src/test/java/com/rameez/ReverseBits.java
@@ -8,12 +8,13 @@ import static org.junit.Assert.assertEquals;
 
 class ReverseBits {
     // you need treat n as an unsigned value
+    // METHOD 1: WITHOUT USING BITWISE OPERATOR
     public int reverseBits(int n) {
         int answer = 0;
 
         // handle 0th index bit of the input.
-        if (n%2 != 0){
-            answer+=Integer.MIN_VALUE;
+        if (n % 2 != 0) {
+            answer += Integer.MIN_VALUE;
         }
 
         // 'n' bits can represent nos from "(-) 2pow(n-1)" to "2pow(n-1) - 1".
@@ -25,12 +26,26 @@ class ReverseBits {
             }
         }
         // handle 31st index bit of the input.
-        if (n < 0){
-            answer+=1;
+        if (n < 0) {
+            answer += 1;
         }
 
         return answer;
     }
+
+    // you need treat n as an unsigned value
+    // METHOD 2: USING BITWISE OPERATORS (faster method)
+    public int reverseBits2(int n) {
+
+        n = (n >>> 16) | (n << 16);
+        n = ((n & 0xff00ff00) >>> 8) | ((n & 0x00ff00ff) << 8);
+        n = ((n & 0xf0f0f0f0) >>> 4) | ((n & 0x0f0f0f0f) << 4);
+        n = ((n & 0xcccccccc) >>> 2) | ((n & 0x33333333) << 2);
+        n = ((n & 0xaaaaaaaa) >>> 1) | ((n & 0x55555555) << 1);
+        return n;
+
+    }
+
 
     @Test
     void test() {
